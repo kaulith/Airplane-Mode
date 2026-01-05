5a05379c8cdd6ebbd7fad22a6e8a7473fadbaa2a
kaulith <kaushalshriwas118@gmail.com>
2026-01-05 17:35:45 +0530

    Feat: Shop Payment Item and Shop Contract Charges Doctype for penalties and scheduler

---
+++ airplane_mode/airport_shop_management/doctype/shop_contract_payment/shop_contract_payment.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2025, Kaushal and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class ShopContractPayment(Document):
+	def validate(self):
+		# Prevent editing paid rows
+		if not self.is_new():
+			old_doc = self.get_doc_before_save()
+			if old_doc and old_doc.status == "Paid":
+				# Allow only cancellation of paid rows
+				if self.status != "Paid":
+					frappe.throw("Cannot modify a paid payment row. Please cancel the related Rent Payment first.")
