87b7c5b11bc025493c6ddba9eb64de0e3c29478e
kaulith <kaushalshriwas118@gmail.com>
2026-01-05 17:28:32 +0530

    feat: filters for crew member and gate number

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.js
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2025, Kaushal and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Flight", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airplane Flight", {
+	refresh(frm) {
+		// Set filter for crew member field to show only active crew
+		frm.set_query('crew_member', 'flight_crew', function(doc, cdt, cdn) {
+			return {
+				filters: {
+					'status': 'Active'
+				}
+			};
+		});
 
-// 	},
-// });
+		// Set filter for gate number field to show only gates from source airport
+		frm.set_query('gate_number', function() {
+			return {
+				filters: {
+					'airport': frm.doc.source_airport
+				}
+			};
+		});
+	}
+});
