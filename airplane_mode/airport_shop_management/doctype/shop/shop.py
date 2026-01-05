adfdb9cf9ef74683d98c3da8b47f5dc3b1dacd7b
kaulith <kaushalshriwas118@gmail.com>
2026-01-05 17:34:39 +0530

    Ref: made shop doctype support web page

--- airplane_mode/airport_shop_management/doctype/shop/shop.py
+++ airplane_mode/airport_shop_management/doctype/shop/shop.py
@@ -2,11 +2,31 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
-class Shop(Document):
-	def before_insert(self):
-		if not self.rent:
-			settings = frappe.get_single("Shop Settings")
-			if settings.default_rent_amount:
-				self.rent = settings.default_rent_amount
+class Shop(WebsiteGenerator):
+    def validate(self):
+        super().validate()
+        
+        if self.shop_area:
+            area = frappe.db.get_value("Shop Area", self.shop_area, "calculated_area")
+            self.area = area
+        
+        # Auto-fill base_rent with default_rent_amount if empty
+        if not self.base_rent:
+            settings = frappe.get_single("Shop Settings")
+            self.base_rent = settings.default_rent_amount or 0
+    
+    def get_context(self, context):
+        # Add lease information if the shop is occupied
+        if self.status == "Occupied":
+            leases = frappe.get_all(
+                "Shop Lease",
+                filters={"shop": self.name, "status": "Active"},
+                fields=["name", "tenant", "tenant_name", "lease_start_date", "lease_end_date"],
+                limit_page_length=1
+            )
+            if leases:
+                context["lease"] = leases[0]
+        
+        return context
