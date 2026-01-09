9b703a6a0e594ad91d159318c3c20553000d18e4
kaulith <kaushalshriwas118@gmail.com>
2026-01-09 13:42:46 +0530

    Feat: convert lead to tenant

--- airplane_mode/airport_shop_management/doctype/shop_lead/shop_lead.js
+++ airplane_mode/airport_shop_management/doctype/shop_lead/shop_lead.js
@@ -1,8 +1,32 @@
 // Copyright (c) 2026, Kaushal and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Shop Lead", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Shop Lead", {
+	refresh(frm) {
+		// Show "Convert to Tenant" button if not already converted
+		if (frm.doc.status !== "Converted" && !frm.doc.tenant && !frm.is_new()) {
+			frm.add_custom_button(__("Convert to Tenant"), function() {
+				frappe.call({
+					method: "airplane_mode.airport_shop_management.doctype.shop_lead.shop_lead.convert_to_tenant",
+					args: {
+						lead_name: frm.doc.name
+					},
+					callback: function(r) {
+						if (r.message) {
+							frm.reload_doc();
+							// Open the new tenant
+							frappe.set_route("Form", "Shop Tenant", r.message);
+						}
+					}
+				});
+			});
+		}
+		
+		// If already converted, show link to tenant
+		if (frm.doc.tenant) {
+			frm.add_custom_button(__("View Tenant"), function() {
+				frappe.set_route("Form", "Shop Tenant", frm.doc.tenant);
+			});
+		}
+	},
+});
