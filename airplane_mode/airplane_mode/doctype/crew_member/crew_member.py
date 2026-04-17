5721539a5ece4ef18889be684878e7a60199da9c
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-04-17 20:56:29 +0530

    refactor: switch crew member to expression naming and clean up boilerplate

--- airplane_mode/airplane_mode/doctype/crew_member/crew_member.py
+++ airplane_mode/airplane_mode/doctype/crew_member/crew_member.py
@@ -11,26 +11,26 @@ class CrewMember(Document):
 			self.full_name = f"{self.first_name} {self.last_name}"
 		else:
 			self.full_name = self.first_name
-		
+
 		self.check_duplicate_crew_member()
-	
+
 	def check_duplicate_crew_member(self):
 		# Don't allow same name + airline combo
 		if not self.first_name or not self.airline:
 			return
-		
+
 		filters = {
 			"first_name": self.first_name,
 			"airline": self.airline,
 			"name": ["!=", self.name]
 		}
-		
+
 		# If last_name exists, include it in the filter
 		if self.last_name:
 			filters["last_name"] = self.last_name
-		
+
 		existing_crew = frappe.db.exists("Crew Member", filters)
-		
+
 		if existing_crew:
 			frappe.throw(
 				f"Crew member '{self.full_name}' already exists in {self.airline}. No duplicates allowed.",
