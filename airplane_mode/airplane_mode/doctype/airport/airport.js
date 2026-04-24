3dad97f26543bc89a781203333d862b53d3c9c85
Kaushal Shriwas <64089478+kaulith@users.noreply.github.com>
2026-04-24 17:13:22 +0530

    feat: add button to fetch list of shops on airport

--- airplane_mode/airplane_mode/doctype/airport/airport.js
+++ airplane_mode/airplane_mode/doctype/airport/airport.js
@@ -1,2 +1,27 @@
 // Copyright (c) 2025, Kaushal and contributors
 // For license information, please see license.txt
+
+frappe.ui.form.on("Airport", {
+
+
+	refresh(frm) {
+
+        frm.add_custom_button("Get shop list", () =>{
+            //shop list with status
+            frappe.db.get_list("Shop", 
+                {
+                    fields: ['shop_name', 'status'],
+                    filters:{'airport': frm.doc.name}
+                }
+            ).then( r => {
+                let ls = '<ol>'
+                for(const i of r){
+                    ls = ls + '<li>' + i.shop_name + ":" + i.status+ '</li>'
+                }
+                ls = ls + '</ol>'
+                frappe.msgprint(ls)
+            })
+        })
+    }
+    }
+)
